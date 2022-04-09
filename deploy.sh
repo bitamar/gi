281bc75770adb0090837b3d5b49056ba76873961
Itamar Bar-Lev <itamar@aporia.com>
2022-04-09 19:30:21 +0300

    deploy to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,23 @@
 # abort on errors
 set -e
 
+git stash
+gco gh-pages
+
 # build
 yarn docs:build
 
 # navigate into the build output directory
-cd docs/.vitepress/dist
+rm -rf dist/
+cp -R docs/.vitepress/dist dist/
 
 # if you are deploying to a custom domain
+cd dist
 # echo 'www.example.com' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:bitamar/gi.git main:gh-pages
+git push -f origin gh-pages
 
 cd -
+gco -
